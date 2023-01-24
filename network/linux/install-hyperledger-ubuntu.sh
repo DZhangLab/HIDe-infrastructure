b1a3074251d2268e49eb30e4205d8cc7b3a3d167
Wyn Van Devanter <wynvandev@gmail.com>
2023-01-24 10:12:59 -0500

    updates to HL install script

--- network/linux/install-hyperledger-ubuntu.sh
+++ network/linux/install-hyperledger-ubuntu.sh
@@ -3,13 +3,14 @@
 # Running this script will install and test Hyperledger Fabric and its dependencies using apt-get, curl and git
 #
 #   REF: https://github.com/DZhangLab/HIDe-infrastructure/tree/main/network/README.md
+set -x 
 
 ##########################################################################################
-# install Docker Engine on Ubuntu. https://docs.docker.com/engine/install/ubuntu/
+# install Docker Engine via apt-get (https://docs.docker.com/engine/install/ubuntu/)
 ##########################################################################################
+set -x # show commands before executing them
 
 # http stuff needed for allowing egress https (i.e. talking to image registry)
-sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
@@ -26,13 +27,14 @@ echo \
 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo docker run hello-world
 
-sudo apt-get install docker-compose 
+sudo apt install docker-compose -y
 
 ##########################################################################################
-# download & install Hyperledger Fabric 
+# download & install Hyperledger Fabric binaries and samples
 ##########################################################################################
 
 sudo chmod 666 /var/run/docker.sock
@@ -44,9 +46,7 @@ curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/boot
 export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 
-git clone https://github.com/DZhangLab/HIDe-infrastructure
-
-cd HIDe-infrastructure/network
+cd fabric-samples/test-network
 
 # start Hyperledger Fabric
 ./network up 
@@ -55,37 +55,8 @@ cd HIDe-infrastructure/network
 ./network.sh createChannel
 
 ##########################################################################################
-# get samples & install chaincode
+# install chaincode
 ##########################################################################################
 
-git clone fabric-samples
-cd fabric-samples/asset-transfer-basic/chaincode-javascript/
-sudo apt-get install npm
-
-##########################################################################################
-# test some ledger operations 
-##########################################################################################
-
-# Environment variables for Org1
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=localhost:7051
-
-# Invoke the chaincode by creating an initial set of assets on the ledger
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"InitLedger","Args":[]}'
-
-# query the test data
-peer chaincode query -C mychannel -n basic -c '{"Args":["GetAllAssets"]}'
-
-# Switch to a different org 
-# Environment variables for Org2
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="Org2MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-export CORE_PEER_ADDRESS=localhost:9051
-
-# query the test data
-peer chaincode query -C mychannel -n basic -c '{"Args":["GetAllAssets"]}'
+cd ../asset-transfer-basic/chaincode-javascript/
+sudo apt install npm -y
